52df6fa2395bbe53b85e32a0c565bb0ea93e8f68
Thein Htut <theinhtut.dec@gmail.com>
2020-01-10 14:03:01 +0800

    Installed react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 import './styles/styles.scss'
 
 const Kaolendar = () => (
   <article>
-    <h5 className='bluetext'>This is from Kaolendar</h5>
+    <h5 className="bluetext">This is from Kaolendar</h5>
+    <Button bsStyle="success">Success</Button>
   </article>
 )
 
