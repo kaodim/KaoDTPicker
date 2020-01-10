d74c732145bd6e4f13232befd75c95042ced8572
Thein Htut <theinhtut.dec@gmail.com>
2020-01-10 13:47:52 +0800

    Setup webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build|dist)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  externals: {
+    react: 'commonjs react'
+  }
+}
