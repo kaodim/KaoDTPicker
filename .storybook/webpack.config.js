4844ead946db96684a21758028cbc3ae84792c9a
Thein Htut <theinhtut.dec@gmail.com>
2020-01-11 15:55:20 +0800

    Updated storybook webpack config file

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -17,53 +17,53 @@
 //   },
 // };
 
-// var path = require('path')
+var path = require('path')
 
-// module.exports = {
-//   // entry: './src/index.js',
-//   entry: {
-//     index: './src/index.js',
-//     app: './src/app.js'
-//   },
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     // filename: 'index.js',
-//     filename: '[name].js',
-//     // libraryTarget: 'commonjs2'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         include: path.resolve(__dirname, 'src'),
-//         exclude: /(node_modules|bower_components|build|dist)/,
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['env']
-//           }
-//         }
-//       },
-//       {
-//         test: /\.s?css$/,
-//         use: [
-//           'style-loader',
-//           'css-loader',
-//           'sass-loader'
-//         ]
-//       }
-//       // {
-//       //   test: /\.(png|svg|jpg|gif)$/,
-//       //   use: [
-//       //     'file-loader'
-//       //   ]
-//       // }
-//     ]
-//   },
-//   // externals: {
-//   //   react: 'commonjs react'
-//   // },
-//   devServer: {
-//     contentBase: path.join(__dirname,'public')
-//   }
-// }
+module.exports = {
+  // entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    app: './src/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    // filename: 'index.js',
+    filename: '[name].js',
+    // libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build|dist)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+      // {
+      //   test: /\.(png|svg|jpg|gif)$/,
+      //   use: [
+      //     'file-loader'
+      //   ]
+      // }
+    ]
+  },
+  // externals: {
+  //   react: 'commonjs react'
+  // },
+  devServer: {
+    contentBase: path.join(__dirname,'public')
+  }
+}
